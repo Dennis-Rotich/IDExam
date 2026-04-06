ee3539f5c91f87a345677954dd3940ff21418436
Ubuntu <ubuntu@ip-10-0-3-92.ec2.internal>
2026-04-06 19:50:53 +0000

    feat: updated file with debug logs

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB = async()=>{
+const connectDB = async () => {
+  console.log("URI:", process.env.MONGODB_URI); // add this temporarily
+  mongoose.connection.on("connected", () => console.log("Database connected"));
+  await mongoose.connect(`${process.env.MONGODB_URI}/IDExam`);
+  console.log(`conns=ection established at ${process.env.MONGODB_URI}/IDExam`);
+};
 
-    mongoose.connection.on('connected',()=>console.log("Database connected"));
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/IDExam`)
-}
-
-export default connectDB; 
+export default connectDB;
