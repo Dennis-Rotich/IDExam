cce9fb40d159ff45a7e94e6d1663a705ef762966
Esaius2058 <wabwirewild@gmail.com>
2026-04-07 15:10:59 +0300

    feat: Consolidate user routes and add admin features

--- backend/routes/studentRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { register, login } from '../controllers/studentController.js';
-
-const studentRouter = express.Router();
-
-studentRouter.post('/register', register)
-studentRouter.post('/login', login)
-
-export default studentRouter;
