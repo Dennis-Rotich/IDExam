d3e37fa4110dcd17c621aa2d8aea9741a764f219
Esaius2058 <wabwirewild@gmail.com>
2026-04-07 15:08:52 +0300

    chore: Add MongoDB connection logs

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   console.log("URI:", process.env.MONGODB_URI); // add this temporarily
   mongoose.connection.on("connected", () => console.log("Database connected"));
   await mongoose.connect(`${process.env.MONGODB_URI}/IDExam`);
-  console.log(`conns=ection established at ${process.env.MONGODB_URI}/IDExam`);
+  console.log(`connection established at ${process.env.MONGODB_URI}/IDExam`);
 };
 
 export default connectDB;
