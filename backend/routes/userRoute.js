41457125370a443de183afd591d2754d0b0ebafc
Esaius2058 <wabwirewild@gmail.com>
2026-04-07 19:05:18 +0300

    feat: Add exam CRUD, autosave, and user endpoints

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,7 +6,8 @@ import {
     undeleteUser, 
     getAllUsers, 
     getUserProfile, 
-    updateUser 
+    updateUser, 
+    deleteUserById
 } from '../controllers/userController.js';
 import { verifyToken, isAdmin } from "../middlewares/auth.js";
 
@@ -20,17 +21,22 @@ userRouter.post('/login', login);
 // ==========================================
 // Get the currently logged-in user's profile
 userRouter.get('/profile', verifyToken, getUserProfile);
-// Update a user's details by ID
-userRouter.put('/:id', verifyToken, updateUser);
+// Update a user's details by ID 
+// updated it to fetch id from the session and not the params
+userRouter.put('/', verifyToken, updateUser);
 
 // Administrative Routes 
 // ==========================================
 // Get all users (Useful for managing the platform)
-userRouter.get('/', verifyToken, isAdmin, getAllUsers);
+userRouter.get('/all', verifyToken, isAdmin, getAllUsers);
 
-// Soft delete a user (Sets isDeleted: true and deletedAt: Date)
-userRouter.delete('/:id', verifyToken, deleteUser);
+// Soft Delete Own Account (No ID in URL needed)
+userRouter.delete('/deactivate', verifyToken, deleteUser);
+// Soft Delete Another User (Admin/System only)
+userRouter.delete('/deactivate/:id', verifyToken, isAdmin, deleteUser);
+// Hard Delete (Strictly Admin only)
+userRouter.delete('/permanent/:id', verifyToken, isAdmin, deleteUserById);
 // Restore a soft-deleted user
-userRouter.patch('/:id/restore', verifyToken, isAdmin, undeleteUser);
+userRouter.patch('/restore/:id', verifyToken, isAdmin, undeleteUser);
 
 export default userRouter;
