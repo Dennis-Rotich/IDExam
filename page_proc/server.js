4583e1d4847b472a4151ba89c4cd5a90f6dbdab2
Morgan Mooka <165254513+troymom@users.noreply.github.com>
2026-04-08 13:59:12 +0300

    changed port to 3500

--- page_proc/server.js
+++ page_proc/server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const path = require('path')
 
 const app = express()
-const PORT = 3000
+const PORT = 3500
 
 // Middleware
 app.use(cors()) // Allows the HTML page to communicate with this server
