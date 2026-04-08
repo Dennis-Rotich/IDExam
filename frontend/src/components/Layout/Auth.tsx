a2e07d7646322950c615605b8a3e46bfdc68124c
Esaius2058 <wabwirewild@gmail.com>
2026-04-08 10:46:08 +0300

    fix: Use min-h-screen for Auth layout

--- frontend/src/components/Layout/Auth.tsx
+++ frontend/src/components/Layout/Auth.tsx
@@ -56,7 +56,7 @@ export function AuthLayout() {
   }
 
   return (
-    <div className="h-screen w-full grid lg:grid-cols-2 text-left">
+    <div className="min-h-screen w-full grid lg:grid-cols-2 text-left">
       {/* LEFT PANEL: Dynamic Branding & Context (Dark Mode) */}
       <div className="h-screen hidden lg:flex flex-col justify-between bg-[#000] p-12 xl:p-16 border-r border-[#333]">
         {/* TOP: Brand & Tagline */}
